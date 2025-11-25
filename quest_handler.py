dab8b70b7b69cc2a7dee39049bc219198bcb422f
Samuel Somerville <sam.mikesomerville@gmail.com>
2025-11-24 22:56:21 -0500

    Update quest_handler.py

--- quest_handler.py
+++ quest_handler.py
@@ -53,7 +53,7 @@ def accept_quest(character, quest_id, quest_data_dict):
 
     if quest_id in quest_data_dict:
         if quest_data_dict[quest_id]["required_level"] <= character["level"]:
-            if quest_data_dict["prerequiste"] == None:
+            if quest_data_dict[quest_id]["prerequiste"] == None:
                 if quest_id not in character["completed_quests"]:
                     if quest_id not in character["active_quests"]:
                         character["active_quests"].append(quest_id)
