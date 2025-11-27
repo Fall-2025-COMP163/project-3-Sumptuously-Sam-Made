15f1baab44d322c4410cf3ec579dc47edad12414
Samuel Somerville <sam.mikesomerville@gmail.com>
2025-11-27 15:03:07 -0500

    Update quest_handler.py

--- quest_handler.py
+++ quest_handler.py
@@ -101,9 +101,9 @@ def complete_quest(character, quest_id, quest_data_dict):
         if quest_id in character["active_quests"]:
             character["active_quests"].remove(quest_id)
             character["completed_quests"].append(quest_id)
-            character_manager.gain_experience(character, quest_data_dict["reward_xp"])
-            character_manager.add_gold(character, quest_data_dict["reward_gold"])
-            return f"{quest_data_dict["reward_xp"]}, {quest_data_dict["reward_gold"]}"
+            character_manager.gain_experience(character, quest_data_dict[quest_id]["reward_xp"])
+            character_manager.add_gold(character, quest_data_dict[quest_id]["reward_gold"])
+            return f"{quest_data_dict[quest_id]["reward_gold"]}, {quest_data_dict[quest_id]["reward_xp"]}"
         else:
             raise QuestNotActiveError
     else:
