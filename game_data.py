1d515c3c17ac046c2cc4bcc279856c8582af1cd4
Samuel Somerville <sam.mikesomerville@gmail.com>
2025-11-29 14:39:05 -0500

    Update game_data.py

--- game_data.py
+++ game_data.py
@@ -55,6 +55,8 @@ def load_quests(filename="data/quests.txt"):
                     if sp_line[0] == "QUEST_ID":
                         key = sp_line[1].strip()
                         data_dict[sp_line[0].lower()] = sp_line[1]
+                     elif sp_line[0] == "REQUIRED_LEVEL":
+                        data_dict[sp_line[0].lower()] = sp_line[1].strip()
                     else:
                         data_dict[sp_line[0].lower()] = sp_line[1]
             return quest_data_dict
