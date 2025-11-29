edb7b59bd36fce4fa5f18781d80f40d76f1f4065
Samuel Somerville <sam.mikesomerville@gmail.com>
2025-11-28 20:14:44 -0500

    Update quest_handler.py

--- quest_handler.py
+++ quest_handler.py
@@ -59,7 +59,25 @@ def accept_quest(character, quest_id, quest_data_dict):
             item[quest] = item[quest].strip()
 
     quest_sp = " " + quest_id
-    if quest_id in quest_data_dict:
+    if quest_sp in quest_data_dict:
+        if int(quest_data_dict[quest_sp]["required_level"]) < (character["level"]):
+            if quest_data_dict[quest_sp]["prerequisite"].upper() == "NONE" or quest_data_dict[quest_sp]["prerequisite"] in character["completed_quests"]:
+                if quest_sp not in character["completed_quests"]:
+                    if quest_sp not in character["active_quests"]:
+                        character["active_quests"].append(quest_sp)
+                        return True
+                    else:
+                        raise QuestNotFoundError
+                else:
+                    raise QuestAlreadyCompletedError
+            else:
+                raise QuestRequirementsNotMetError
+        else:
+            raise InsufficientLevelError
+    else:
+        raise QuestNotFoundError
+        
+    else:
         if int(quest_data_dict[quest_id]["required_level"]) < (character["level"]):
             if quest_data_dict[quest_id]["prerequisite"].upper() == "NONE" or quest_data_dict[quest_id]["prerequisite"] in character["completed_quests"]:
                 if quest_id not in character["completed_quests"]:
